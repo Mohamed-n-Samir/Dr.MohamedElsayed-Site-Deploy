d6fb47cc4092cfb4ff44ba9d52585e54e4a5d9ea
Mohamed Samir <mohamed.n.work12@gmail.com>
2023-02-17 15:11:14 +0200

    finish developing all headers for all routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,57 @@ import Header from "./Components/Header/Header";
 import Navbar1 from "./Components/Navbar1/Navbar1";
 import Footer from "./Components/Footer/Footer";
 
-
 function App() {
 	return (
 		<div>
-			<Navbar1/>
-			<Header/>
+			<Navbar1 />
+			<Header />
 			<Routes>
 				<Route path="/login" element={<div>login</div>} exact />
 				{/* <Route path="/login" element={<div>login</div>} exact /> */}
-				<Route path="/profile" element={<div>profile</div>} exact />
-				<Route path="/vlogs" element={<div>vlogs</div>} exact />
-				<Route path="/blogs" element={<div>blogs</div>} exact />
-				<Route path="/liveBroadcast" element={<div>live-broadcast</div>} exact />
-				<Route path="/lectures" element={<div>lectures</div>} exact />
-				<Route path="/profile" element={<div>profile</div>} exact />
-				<Route path="/buyCredit" element={<div>buy-credit</div>} exact />
-				{["/home", "/"].map((path, index) => (
+				<Route
+					path="/Dr.MohamedElsayed-Site-Deploy/profile"
+					element={<div>profile</div>}
+					exact
+				/>
+				<Route
+					path="/Dr.MohamedElsayed-Site-Deploy/vlogs"
+					element={<div>vlogs</div>}
+					exact
+				/>
+				<Route
+					path="/Dr.MohamedElsayed-Site-Deploy/blogs"
+					element={<div>blogs</div>}
+					exact
+				/>
+				<Route
+					path="/Dr.MohamedElsayed-Site-Deploy/liveBroadcast"
+					element={<div>live-broadcast</div>}
+					exact
+				/>
+				<Route
+					path="/Dr.MohamedElsayed-Site-Deploy/lectures"
+					element={<div>lectures</div>}
+					exact
+				/>
+				<Route
+					path="/Dr.MohamedElsayed-Site-Deploy/profile"
+					element={<div>profile</div>}
+					exact
+				/>
+				<Route
+					path="/Dr.MohamedElsayed-Site-Deploy/buyCredit"
+					element={<div>buy-credit</div>}
+					exact
+				/>
+				{[
+					"/Dr.MohamedElsayed-Site-Deploy/home",
+					"/Dr.MohamedElsayed-Site-Deploy/",
+				].map((path, index) => (
 					<Route path={path} element={<Home />} key={index} />
 				))}
 			</Routes>
-			<Footer/>
+			<Footer />
 		</div>
 	);
 }
